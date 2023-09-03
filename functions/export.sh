f1ea4c793d811f6199b976050f8a919ea8774a37
Vladimir Alexeev <9141778236@mai.ru>
2023-09-04 08:35:32 +1000

    2.3.3   Исправление мелких багов

--- functions/export.sh
+++ functions/export.sh
@@ -10,6 +10,7 @@ export_instance() {
   is_running=$(is_running_instance ${CONTAINER_NAME})
   archive_name="$(get_part_from_container_name ${CONTAINER_NAME} h)-$(get_part_from_container_name ${CONTAINER_NAME})-image-container.tar.gz"
   debug "archive_name: ${archive_name}"
+  
   # ловушка перед lxc stop и lxc export
   hook_before_export=${hook_before_export:='before_export'}
   hook_dispath "${hooks_file}" "${hook_before_export}"
@@ -19,8 +20,8 @@ export_instance() {
   [[ -e ${where_copy} ]] || mkdir -p ${where_copy}
   mv -f -t ${where_copy} ${archive_name}
   [[ $DEBUG_LEVEL -lt 90 ]] && [[ $is_running -eq 1 ]] && $lxc_cmd start ${CONTAINER_NAME}
+  
   # ловушка после lxc export и lxc start
   hook_after_export=${hook_after_export:='after_export'}
   hook_dispath "${hooks_file}" "${hook_after_export}"
 }
-
