f239ae3b228e30f6f911d54948bc9b1c219677fd
Vladimir Alexeev <9141778236@mail.ru>
2024-06-15 16:46:23 +1000

    v2.3.19 instance nagios change backup.sh

--- instances/nagios/backup.sh
+++ instances/nagios/backup.sh
@@ -13,7 +13,7 @@ name_tar=/root/named.tar.gz
 #force_backup=1
 
 # /etc/bind
-lxc -q exec ${CONTAINER_NAME} -- sh -c "tar -czf ${name_tar} /etc/apache2/conf-available/nagios4-cgi.conf /etc/apache2/mods-available/mime.conf /etc/nagios4/* /root/.config/* /root/.ssh/* /root/.bash_history /root/.bashrc /root/.profile > /dev/null"
+lxc -q exec ${CONTAINER_NAME} -- sh -c "tar -czf ${name_tar} /etc/apache2/conf-available/nagios4-cgi.conf /etc/apache2/mods-available/mime.conf /etc/nagios4/* /usr/lib/nagios/*.sh /etc/exim4/* /root/.config/* /root/.ssh/* /root/.bash_history /root/.bashrc /root/.profile > /dev/null"
 if [[ $? -eq 0 ]]; then
   lxc file pull -q -p ${CONTAINER_NAME}${name_tar} ${where_copy}
   if [[ $? -ne 0 ]]; then
