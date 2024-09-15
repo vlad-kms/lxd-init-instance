ac7728a69cb6bdec86a7a7af6b69af3909315c07
Vladimir Alexeev <9141778236@mail.ru>
2024-09-15 10:31:47 +1000

    v3.1.2 мелкие исправления

--- instances/nagios/backup.sh
+++ instances/nagios/backup.sh
@@ -16,16 +16,20 @@ name_tar="${name_tar:=${dt}named.tar.gz}"
 name_tar="/root/$name_tar"
 echo "$0 - name_tar: $name_tar"
 
-lxc -q exec "${CONTAINER_NAME}" -- sh -c "tar -czf ${name_tar} /etc/apache2/conf-available/nagios4-cgi.conf \
+# shellcheck disable=SC2154
+$lxc_cmd -q exec "${CONTAINER_NAME}" -- sh -c "tar -czf ${name_tar} /etc/apache2/conf-available/nagios4-cgi.conf \
     /etc/apache2/mods-available/mime.conf /etc/nagios4/* /usr/lib/nagios/*.sh /etc/exim4/* /root/.config/* \
     /root/.ssh/* /root/.bash_history /root/.bashrc /root/.profile /usr/share/nagios4/htdocs/images/logos/eve/* > /dev/null 2> /dev/null"
-if [[ $? -eq 0 ]]; then
+ret=$?
+if [[ $ret -eq 0 ]]; then
   # shellcheck disable=SC2154
-  lxc file pull -q -p "${CONTAINER_NAME}${name_tar}" "${where_copy}"
-  if [[ $? -ne 0 ]]; then
+  $lxc_cmd file pull -q -p "${CONTAINER_NAME}${name_tar}" "${where_copy}"
+  ret=$?
+  if [[ $ret -ne 0 ]]; then
     ret_message="Ошибка при копировании файлов"
     ret_code=1000
   fi
+  $lxc_cmd exec "${CONTAINER_NAME}" -- rm "${name_tar}"
 else
   ### ошибка поиска файлов
   # shellcheck disable=SC2034
--- instances/ngs/backup.sh
+++ instances/ngs/backup.sh
@@ -19,18 +19,20 @@ name_tar="${name_tar:=${dt}named.tar.gz}"
 name_tar="/root/$name_tar"
 #debug "$0 - name_tar: $name_tar"
 
-lxc -q exec "${CONTAINER_NAME}" -- sh -c "tar -czf ${name_tar} /etc/nagios/* /usr/lib/nagios/*.sh /root/.config/* \
+# shellcheck disable=SC2154
+$lxc_cmd -q exec "${CONTAINER_NAME}" -- sh -c "tar -czf ${name_tar} /etc/nagios/* /usr/lib/nagios/*.sh /root/.config/* \
     /root/.ssh/* /root/.bash_history /root/.bashrc /root/.profile /usr/share/nagios/htdocs/images/logos/eve/* > /dev/null 2> /dev/null"
 ret=$?
 if [[ $ret -eq 0 ]]; then
   # shellcheck disable=SC2154
-  lxc file pull -q -p "${CONTAINER_NAME}${name_tar}" "${where_copy}"
+  $lxc_cmd file pull -q -p "${CONTAINER_NAME}${name_tar}" "${where_copy}"
   ret=$?
   ret_message="Успешное копирование файлов ${CONTAINER_NAME}${name_tar}"
   if [[ $ret -ne 0 ]]; then
     ret_message="Ошибка при копировании файлов"
     ret_code=1000
   fi
+  $lxc_cmd exec "${CONTAINER_NAME}" -- rm "${name_tar}"
 else
   ### ошибка поиска файлов
   #set_ret_message "Ошибка поиска файлов"
