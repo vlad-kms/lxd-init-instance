f47544a886dac10ef34c35ab59b4f2354ab852c7
Vladimir Alexeev <9141778236@mail.ru>
2024-10-11 06:35:34 +1000

    v3.1.13 fix

--- instances/dev-java/_hooks.sh
+++ instances/dev-java/_hooks.sh
@@ -6,6 +6,7 @@ after_init_container() {
 
 after_start() {
   debug "DISPATH HOOK: Run ${0}:hook-afterstart::after_start"
+  # shellcheck disable=SC2154
   debug "lxc_cmd: ${lxc_cmd}"
   cmd="${lxc_cmd} exec ${CONTAINER_NAME} -- "
 
